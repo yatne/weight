1d83937f007b167ea7efd2ec7e9622be6c2d9748
Antoni Smoliński <antek.smolinski@senfino.net>
2018-03-18 16:54:08 +0100

    simple controller

---
+++ backend/src/main/java/cbr/model/Weight.java
@@ -0,0 +1,41 @@
+package cbr.model;
+
+import java.util.Date;
+
+public class Weight {
+
+    private int id;
+    private Date date;
+    private float weight;
+
+    public Weight(int id, float weight, Date date) {
+        this.id = id;
+        this.weight = weight;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+}
