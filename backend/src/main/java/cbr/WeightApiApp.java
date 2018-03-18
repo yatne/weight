96678391f766decdb82d3db8bd61cfe4192dc1f8
Antoni Smoliński <antek.smolinski@senfino.net>
2018-03-18 15:41:09 +0100

    springboot starter

--- backend/src/main/java/cbr/WeightApiApp.java
+++ backend/src/main/java/cbr/WeightApiApp.java
@@ -1,6 +1,12 @@
-public class weightApiApp {
+package cbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightApiApp {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(WeightApiApp.class, args);
     }
 }
