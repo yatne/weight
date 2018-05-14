5d73f06fdbac69b5ab18a2db045cd34c51e9f252
Antoni Smoliński <antek.smolinski@senfino.com>
2018-05-14 14:23:43 +0200

    few new colors

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
 "use strict";
 
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class Header extends React.Component {
 
   render() {
     return (
       <div className='header'>
-        HEADER
+        <div className="header-title">
+          GRUBOŚĆ
+        </div>
+        <div className="header-menu">
+          <ul>
+            <li><Link to="/main">Main</Link></li>
+            <li><Link to="/report">Report</Link></li>
+          </ul>
+        </div>
       </div>
     );
   }
