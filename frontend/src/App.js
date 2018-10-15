e48ecf5e41f2d8ec99f7fe9ac58b7b8d73e54924
Antoni <antonimaciej@gmail.com>
2018-10-15 18:48:25 +0200

    Refactored Central Layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,13 @@ import {
 
 import ReportDashboard from './components/ReportDashboard/ReportDashboard';
 import MainDashboard from './components/MainDashboard/MainDashboard';
-import CentralLayoutScreen from './components/Screen/CentralLayoutScreen';
-
-const Screen = () => (
-  <CentralLayoutScreen><MainDashboard /></CentralLayoutScreen>
-);
+import withCentralLayout from './components/Screen/withCentralLayout';
 
 const Routes = () => (
   <Router>
     <div>
       <Route exact path="/report" component={ReportDashboard} />
-      <Route exact path="/" component={Screen} />
+      <Route exact path="/" component={withCentralLayout(MainDashboard)} />
     </div>
   </Router>
 );
--- frontend/src/components/Screen/withCentralLayout.js
+++ frontend/src/components/Screen/withCentralLayout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header';
+import style from './centralLayoutScreen.scss';
 
 export default function withCentralLayout(Screen) {
   return () => (
     <div className="app">
       <Header />
-      <div className="central-layout">
+      <div className={style.centralLayout}>
         <Screen />
       </div>
     </div>
