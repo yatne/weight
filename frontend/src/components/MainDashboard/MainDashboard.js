0f05f9f2f6340eec377a4a72c609ca3017724c85
Antoni <antonimaciej@gmail.com>
2018-10-15 18:54:22 +0200

    changed MainScreen default behavior

--- frontend/src/components/MainDashboard/MainDashboard.js
+++ frontend/src/components/MainDashboard/MainDashboard.js
@@ -1,27 +1,29 @@
 import React from 'react';
 
-import WeightManagementPanel from './WeightManagementPanel';
 import DefaultChartsPanel from './DefaultChartsPanel';
-import { fetchWeights } from '../../modules/api/weightApi';
+import { fetchThisMonthsWeights } from '../../modules/api/weightApi';
 
 class MainDashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      weights: null,
+      weights: [],
     };
   }
 
-  getWeightsForDates(from, to) {
-    fetchWeights(new Date(from), new Date(to))
+  getThisMonthsDates() {
+    fetchThisMonthsWeights()
       .then(weights => this.setState({ weights }));
+  }
+
+  componentDidMount() {
+    this.getThisMonthsDates();
   }
 
   render() {
     return (
       <div>
-        <WeightManagementPanel onSubmit={(from, to) => this.getWeightsForDates(from, to)} />
-        {!!this.state.weights && this.state.weights.map(weight => (<div>{weight.weight}</div>))}
+        {this.state.weights.map(weight => (<div>{weight.weight}</div>))}
         <DefaultChartsPanel />
       </div>
     );
--- frontend/src/modules/api/weightApi.js
+++ frontend/src/modules/api/weightApi.js
@@ -8,6 +8,12 @@ export function fetchWeights(from, to) {
   `)
     .then(res => res.json());
 }
+
+export function fetchThisMonthsWeights() {
+  return fetch('http://192.168.0.21:8080/weights')
+    .then(res => res.json());
+}
+
 export function addWeight(weight, date = moment()) {
 
 }
