65ce5ba60c3d15ed612a4d3cbc6df367144ad30b
Antoni <antonimaciej@gmail.com>
2021-05-31 19:41:02 +0200

    blah

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import MainDashboard from './components/MainDashboard/MainDashboard';
 import withCentralLayout from './components/Screen/withCentralLayout';
 
 const Routes = () => (
-  <Router basename="/weights">
+  <Router>
     <div>
       <Route exact path="/report" component={withCentralLayout(ReportDashboard)} />
       <Route exact path="/" component={withCentralLayout(MainDashboard)} />
